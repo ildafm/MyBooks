f75c6e9d758401e0bbe06e51b08dfd9860a80dc4
ferdian25 <ildafm4000@gmail.com>
2022-10-24 13:20:58 +0700

    Create BukuViewAdapter.java
    Only Implement

---
+++ MyBooksFadli/app/src/main/java/com/if5a/mybooksfadli/adapters/BukuViewAdapter.java
@@ -0,0 +1,27 @@
+package com.if5a.mybooksfadli.adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BukuViewAdapter extends RecyclerView.Adapter<BukuViewAdapter.viewHolder> {
+    @NonNull
+    @Override
+    public BukuViewAdapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BukuViewAdapter.viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder {
+    }
+}
